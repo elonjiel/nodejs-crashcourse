2c34cf1de51250a00e38f1a3fb87745f6387a22a
Elon Jiel <elonudemy@gmail.com>
2024-05-27 21:25:21 +0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,19 @@
+import http from "http";
+
+const PORT = 8000;
+const server = http.createServer((req, res)=> {
+    // res.setHeader("Content-Type", "text/html");
+    // res.statusCode = 404;
+
+    res.writeHead(500, { "Content-type": "application/json" });
+
+    res.end(JSON.stringify({ message: "Server Error" })); 
+
+ });
+
+
+ server.listen(PORT, () => {
+    console.log(`Server Running on port ${PORT}`)
+ });
+
+
